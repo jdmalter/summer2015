95bcb43d173ac829928900455ddf36584cb85d3e
jdmalter <jdmalter@ncsu.edu>
2015-08-07 23:28:45 -0400

    Added randomized quicksort.

--- Utilities/src/collection/Arrays.java
+++ Utilities/src/collection/Arrays.java
@@ -1,5 +1,7 @@
 package collection;
 
+import java.util.Random;
+
 /**
  * Auxilary class containing static operations for arrays.
  * 
@@ -8,7 +10,37 @@ package collection;
  */
 public class Arrays {
 
+	private static final int QUICK_TO_INSERT_SORT = 24;
+
 	private Arrays() {
+	}
+
+	/**
+	 * Finds the index of a given element in a given sorted array.
+	 * 
+	 * PRECONDITION: Array is sorted from least to greatest, and elements are
+	 * comparable.
+	 * 
+	 * @param array
+	 *            containing elements
+	 * @param element
+	 *            target being found in array
+	 * @return index of element in array, -1 if not found
+	 */
+	public static <T> int binarySearch(T[] array, T element) {
+		return binarySearch(array, element, 0, array.length - 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> int binarySearch(T[] array, T element, int min, int max) {
+		if (array.length < 1 || max < min)
+			return -1;
+		int half = (min + max) / 2;
+		int comparison = (((Comparable<Object>) array[half]).compareTo(element));
+
+		return comparison < 0 ? binarySearch(array, element, half + 1, max)
+				: (comparison == 0 ? half : binarySearch(array, element, min,
+						half - 1));
 	}
 
 	/**
@@ -28,6 +60,8 @@ public class Arrays {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> void insertionSort(T[] array) {
+		if (array.length < 2)
+			return;
 		for (int i = 2; i < array.length; i++) {
 			for (int j = i; j > 1
 					&& ((Comparable<Object>) array[j]).compareTo(array[j - 1]) < 0; j--) {
@@ -63,8 +97,8 @@ public class Arrays {
 		for (int i = 0; i < half; i++)
 			firstHalf[i] = array[i];
 		T[] secondHalf = (T[]) new Object[array.length - half];
-		for (int i = half; i < array.length; i++)
-			secondHalf[i] = array[i];
+		for (int i = 0; i < array.length - half; i++)
+			secondHalf[i] = array[i + half];
 
 		mergeSort(firstHalf);
 		mergeSort(secondHalf);
@@ -77,6 +111,34 @@ public class Arrays {
 				array[i] = firstHalf[j++];
 			else if (k < secondHalf.length && j >= firstHalf.length)
 				array[i] = secondHalf[k++];
+	}
+
+	/**
+	 * Cuts an array into two parts. Elements at the front less than the pivot
+	 * and elements at the end greater than the pivot. Ideally, each part is
+	 * 50/50 split. 10/90 split is bounded by O(nlog(n)). Worst case, the pivot
+	 * is at the end.
+	 * 
+	 * This implementation picks its pivot at random.
+	 * 
+	 * @param array
+	 *            target array being partitioned
+	 * @param first
+	 *            where first part starts
+	 * @param last
+	 *            where second part starts
+	 * @return pivot index
+	 */
+	@SuppressWarnings("unchecked")
+	private static <T> int partition(T[] array, int first, int last) {
+		swap(array, first, last - 1);
+		T pivot = array[new Random().nextInt(last)];
+		int i = first;
+		for (int j = first + 1; j < last; j++)
+			if (((Comparable<Object>) array[j]).compareTo(pivot) <= 0)
+				swap(array, ++i, j);
+		swap(array, first, i);
+		return i;
 	}
 
 	/**
@@ -102,6 +164,21 @@ public class Arrays {
 	}
 
 	/**
+	 * Reverses elements within a given array using (n / 2) swaps where n is the
+	 * number of element.
+	 * 
+	 * @param array
+	 *            target array with elements
+	 */
+	@SuppressWarnings("unused")
+	private static <T> void reverse(T[] array) {
+		if (array.length < 2)
+			return;
+		for (int i = 0; i < array.length / 2; i++)
+			swap(array, i, array.length - i - 1);
+	}
+
+	/**
 	 * Generally slower than any other sort algorithm. Its main advantage is
 	 * guaranteed O(n) swaps. If swaps are expensive, selection sort is ideal.
 	 * Reasonable on small arrays.
@@ -119,6 +196,8 @@ public class Arrays {
 	 */
 	@SuppressWarnings("unchecked")
 	public static <T> void selectionSort(T[] array) {
+		if (array.length < 2)
+			return;
 		for (int i = 0; i < array.length; i++) {
 			int min = i;
 			for (int j = i + 1; j < array.length; j++) {
@@ -145,6 +224,43 @@ public class Arrays {
 		T temp = array[firstIndex];
 		array[firstIndex] = array[lastIndex];
 		array[lastIndex] = temp;
+	}
+
+	/**
+	 * Reasonable as general purpose sorting. Relies on insertion sort for small
+	 * lists. Uses randomized pivots to reduce worse-case scenarios. With
+	 * tuning, runs much faster than mergesort.
+	 * 
+	 * Stability: Equal elements are changed.
+	 * 
+	 * Extra space: O(1)
+	 * 
+	 * Comparisons: O(n^2) but typically O(nlog(n))
+	 * 
+	 * Swaps: O(n^2) but typically O(nlog(n))
+	 * 
+	 * @param array
+	 *            target array with elements
+	 */
+	public static <T> void quickSort(T[] array) {
+		quickSort(array, 0, array.length);
+	}
+
+	private static <T> void quickSort(T[] array, int first, int last) {
+		if (array.length < 2)
+			return;
+		if (array.length < QUICK_TO_INSERT_SORT)
+			insertionSort(array);
+		else if (first < last) {
+			int pivot = partition(array, first, last);
+			if (pivot < array.length / 2) {
+				quickSort(array, first, pivot - 1);
+				quickSort(array, pivot + 1, last);
+			} else {
+				quickSort(array, pivot + 1, last);
+				quickSort(array, first, pivot - 1);
+			}
+		}
 	}
 
 }
--- Utilities/src/collection/Collections.java
+++ Utilities/src/collection/Collections.java
@@ -1,5 +1,7 @@
 package collection;
 
+import java.util.Random;
+
 /**
  * Auxilary class containing static operations for collections.
  * 
@@ -8,11 +10,43 @@ package collection;
  */
 public class Collections {
 
+	private static final int QUICK_TO_INSERT_SORT = 24;
+
 	private Collections() {
 	}
 
 	/**
-	 * Sorts an array by swapping elements until an element is less than its
+	 * Finds the index of a given element in a given sorted list.
+	 * 
+	 * PRECONDITION: List is sorted from least to greatest, and elements are
+	 * comparable.
+	 * 
+	 * @param list
+	 *            containing elements
+	 * @param element
+	 *            target being found in list
+	 * @return index of element in list, -1 if not found
+	 */
+	public static <T> int binarySearch(List<T> list, T element) {
+		return binarySearch(list, element, 0, list.size() - 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> int binarySearch(List<T> list, T element, int min,
+			int max) {
+		if (list.size() < 1 || max < min)
+			return -1;
+		int half = (min + max) / 2;
+		int comparison = (((Comparable<Object>) list.get(half))
+				.compareTo(element));
+
+		return comparison < 0 ? binarySearch(list, element, half + 1, max)
+				: (comparison == 0 ? half : binarySearch(list, element, min,
+						half - 1));
+	}
+
+	/**
+	 * Sorts an list by swapping elements until an element is less than its
 	 * previous swapped element or at the beginning. Reasonable on smaller (less
 	 * than few dozen) lists.
 	 * 
@@ -20,7 +54,7 @@ public class Collections {
 	 * 
 	 * Extra Space: O(1)
 	 * 
-	 * Comparisons and Swaps: Performs well on nearly sorted arrays. Performs
+	 * Comparisons and Swaps: Performs well on nearly sorted lists. Performs
 	 * worst on reversed lists. Therefore adaptive. O(n) to O(n^2)
 	 * 
 	 * @param list
@@ -47,8 +81,8 @@ public class Collections {
 	 * Extra Space: O(n)
 	 * 
 	 * Comparisons and Swaps: Performs moderately well on all types of sorted
-	 * arrays. Minimum swaps on already sorted lists. Maximum swaps on for
-	 * random lists. Not adaptive. O(nlog(n))
+	 * lists. Minimum swaps on already sorted lists. Maximum swaps on for random
+	 * lists. Not adaptive. O(nlog(n))
 	 * 
 	 * @param list
 	 *            subject of mergeSort
@@ -59,7 +93,7 @@ public class Collections {
 			return;
 		int half = list.size() / 2;
 
-		// suppress safe since only elements of type T will be copied into array
+		// suppress safe since only elements of type T will be copied into list
 		List<T> firstHalf = new CircularArray<T>(half);
 		for (int i = 0; i < half; i++)
 			firstHalf.set(i, list.get(i));
@@ -81,10 +115,38 @@ public class Collections {
 	}
 
 	/**
+	 * Cuts an list into two parts. Elements at the front less than the pivot
+	 * and elements at the end greater than the pivot. Ideally, each part is
+	 * 50/50 split. 10/90 split is bounded by O(nlog(n)). Worst case, the pivot
+	 * is at the end.
+	 * 
+	 * This implementation picks its pivot at random.
+	 * 
+	 * @param list
+	 *            target list being partitioned
+	 * @param first
+	 *            where first part starts
+	 * @param last
+	 *            where second part starts
+	 * @return pivot index
+	 */
+	@SuppressWarnings("unchecked")
+	private static <T> int partition(List<T> list, int first, int last) {
+		swap(list, first, last - 1);
+		T pivot = list.get(new Random().nextInt(last));
+		int i = first;
+		for (int j = first + 1; j < last; j++)
+			if (((Comparable<Object>) list.get(j)).compareTo(pivot) <= 0)
+				swap(list, ++i, j);
+		swap(list, first, i);
+		return i;
+	}
+
+	/**
 	 * Checks if given indices are valid for a given collection length. Throws
 	 * exceptions if an index is invalid.
 	 * 
-	 * @param array
+	 * @param list
 	 *            target collection with elements
 	 * @param indexFirst
 	 *            position of first element
@@ -96,9 +158,24 @@ public class Collections {
 			throw new IllegalArgumentException("firstIndex (" + firstIndex
 					+ ") is greater than lastIndex (" + lastIndex + ")");
 		if (lastIndex > listLength)
-			throw new ArrayIndexOutOfBoundsException(lastIndex);
+			throw new IndexOutOfBoundsException();
 		if (firstIndex < 0)
-			throw new ArrayIndexOutOfBoundsException(firstIndex);
+			throw new IndexOutOfBoundsException();
+	}
+
+	/**
+	 * Reverses elements within a given list using (n / 2) swaps where n is the
+	 * number of element.
+	 * 
+	 * @param list
+	 *            target list with elements
+	 */
+	@SuppressWarnings("unused")
+	private static <T> void reverse(List<T> list) {
+		if (list.size() < 2)
+			return;
+		for (int i = 0; i < list.size() / 2; i++)
+			swap(list, i, list.size() - i - 1);
 	}
 
 	/**
@@ -130,11 +207,11 @@ public class Collections {
 	}
 
 	/**
-	 * Swaps elements at two given indices within an array. Last index must be
+	 * Swaps elements at two given indices within an list. Last index must be
 	 * greater than first index.
 	 * 
-	 * @param array
-	 *            target array with elements
+	 * @param list
+	 *            target list with elements
 	 * @param indexFirst
 	 *            position of first element
 	 * @param indexSecond
@@ -145,6 +222,43 @@ public class Collections {
 		T temp = list.get(firstIndex);
 		list.set(firstIndex, list.get(secondIndex));
 		list.set(secondIndex, temp);
+	}
+
+	/**
+	 * Reasonable as general purpose sorting. Relies on insertion sort for small
+	 * lists. Uses randomized pivots to reduce worse-case scenarios. With
+	 * tuning, runs much faster than mergesort.
+	 * 
+	 * Stability: Equal elements are changed.
+	 * 
+	 * Extra space: O(1)
+	 * 
+	 * Comparisons: O(n^2) but typically O(nlog(n))
+	 * 
+	 * Swaps: O(n^2) but typically O(nlog(n))
+	 * 
+	 * @param list
+	 *            target list with elements
+	 */
+	public static <T> void quickSort(List<T> list) {
+		quickSort(list, 0, list.size());
+	}
+
+	private static <T> void quickSort(List<T> list, int first, int last) {
+		if (list.size() < 2)
+			return;
+		if (list.size() < QUICK_TO_INSERT_SORT)
+			insertionSort(list);
+		else if (first < last) {
+			int pivot = partition(list, first, last);
+			if (pivot < list.size() / 2) {
+				quickSort(list, first, pivot - 1);
+				quickSort(list, pivot + 1, last);
+			} else {
+				quickSort(list, pivot + 1, last);
+				quickSort(list, first, pivot - 1);
+			}
+		}
 	}
 
 }
